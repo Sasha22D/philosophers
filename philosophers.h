ba34483e58c8b6e9b0c787627bb0bbf85cc8f532
Sasha Daniel <sadaniel@f4r10s8.clusters.42paris.fr>
2026-01-30 18:39:43 +0100

    blabla

--- philosophers.h
+++ philosophers.h
@@ -1,35 +1,37 @@
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/time.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
 
 typedef struct s_data
 {
-	long	start_time;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	int		has_a_philo_died;
+	long			start_time;
+	long			actual_time;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	int				has_a_philo_died;
+	pthread_mutex_t	print_mutex;
 }				t_data;
 
 typedef struct s_fork
 {
 	int				id;
-	pthread_mutex_t fork_mutex;
+	pthread_mutex_t	fork_mutex;
 }				t_fork;
 
 typedef struct s_thread
 {
-	int			id;
-	pthread_t	philo;
-	t_fork		*fork_left;
-	t_fork		*fork_right;
-	t_data		*data;
-	long		last_meal;
+	int				id;
+	pthread_t		philo;
+	t_fork			*fork_left;
+	t_fork			*fork_right;
+	t_data			*data;
+	long			last_meal;
 }				t_thread;
 
 typedef struct s_monitor
@@ -40,19 +42,19 @@ typedef struct s_monitor
 }				t_monitor;
 
 // THREAD ROUTINE ETC
-void		take_forks(t_thread *philo, long now);
+void		take_forks(t_thread *philo);
 void		drop_forks(t_thread *philo);
 void		*philo_routine(void *args);
 
 // INIT FUNCTIONS
 void		init_data(t_data *data);
-t_thread	**init_struct_array(int	count, t_data *data);
+t_thread	**init_struct_array(int count, t_data *data);
 void		create_pthread(t_thread **philo_array);
 t_fork		**init_fork_array(int count);
 void		assign_forks(t_fork **fork_array, t_thread **philo_array, int count);
 
 // UTILS
-long	get_time();
-void	ft_usleep(int sleep);
+long		get_time(void);
+void		ft_usleep(int sleep);
 
 #endif

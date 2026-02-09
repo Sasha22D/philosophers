ec68af0af3f9a8bc94ba78e5364f174f906a86cb
Sasha Daniel <sadaniel@f2r7s17.clusters.42paris.fr>
2026-02-09 17:53:23 +0100

    normed monitor

--- monitor_thread.c
+++ monitor_thread.c
@@ -16,29 +16,25 @@ int	check_meals(t_thread **philo_array)
 
 void	*monitor_routine(void *args)
 {
-	t_monitor	*monitor = (t_monitor *)args;
-	int	i;
+	t_monitor	*monitor;
+	int			i;
 
+	monitor = (t_monitor *)args;
 	while (monitor->data->has_a_philo_died == 0)
 	{
 		i = 0;
-		monitor->data->actual_time = get_time() - monitor->data->start_time;
 		if (check_meals(monitor->philo_array) == 1)
-		{
 			monitor->data->has_a_philo_died = 1;
-			pthread_mutex_lock(&monitor->data->print_mutex);
-		}
 		while (monitor->philo_array[i] && monitor->data->has_a_philo_died == 0)
 		{
-			if (monitor->philo_array[i]->meals_left != 0)
+			if (get_time() - monitor->philo_array[i]->last_meal >= \
+				monitor->data->time_to_die \
+				&& monitor->philo_array[i]->meals_left != 0)
 			{
-				if (get_time() - monitor->philo_array[i]->last_meal >= monitor->data->time_to_die)
-				{
-					monitor->data->has_a_philo_died = 1;
-					pthread_mutex_lock(&monitor->data->print_mutex);
-					printf("%ld %d died\n", monitor->data->actual_time, monitor->philo_array[i]->id);
-					// pthread_mutex_unlock(&monitor->data->print_mutex);
-				}
+				monitor->data->has_a_philo_died = 1;
+				pthread_mutex_lock(&monitor->data->print_mutex);
+				printf("%ld %d died\n", monitor->data->actual_time, \
+					monitor->philo_array[i]->id);
 			}
 			i++;
 		}
@@ -47,13 +43,13 @@ void	*monitor_routine(void *args)
 	return (NULL);
 }
 
-t_monitor	*init_monitor(t_monitor *monitor, t_data *data, t_thread **philo_array)
+t_monitor	*init_monitor(t_monitor *monitor, t_data *data, t_thread **philo)
 {
 	monitor = malloc(sizeof(t_monitor));
 	if (!monitor)
 		return (NULL);
 	monitor->data = data;
-	monitor->philo_array = philo_array;
+	monitor->philo_array = philo;
 	pthread_create(&monitor->monitor_thread, NULL, monitor_routine, monitor);
 	return (monitor);
 }
